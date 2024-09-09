7976847463a93867560ef6a85ba4c9c9e6f3a230
= <=>
2024-09-09 11:53:47 +0100

    initial commit for game dev using flutter

---
+++ lib/game.dart
@@ -0,0 +1,33 @@
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+import 'player.dart';
+
+class SpaceShooterGame extends FlameGame with PanDetector {
+  late Player player;
+  
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    player = Player()
+      ..position = size / 2
+      ..width = 50
+      ..height = 100
+      ..anchor = Anchor.center;
+
+    add(player);
+  }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    player.move(info.delta.global);
+  }
+
+}
+
+void main() {
+  runApp(GameWidget(game: SpaceShooterGame()));
+}
+
